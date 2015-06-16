1f31bdbff7dfcc1f4fe2832a70a2ae721f41555a
Chris <claymanchris@gmail.com>
2015-06-16 16:53:27 -0700

    error fix

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 5000);
+var port = process.env.PORT || 5000;
 mongoose.connect('mongodb://localhost/hackreactioner');
 
 
